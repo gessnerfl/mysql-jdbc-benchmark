cfaf28c8932049ceedf11f37644fcbdb91bf5e94
Florian Gessner <flo.gessner@gmail.com>
2017-05-21 15:48:22 +0200

    clean up imports

--- src/main/java/de/gessnerfl/mysqljdbcbenchmark/service/ReportCreator.java
+++ src/main/java/de/gessnerfl/mysqljdbcbenchmark/service/ReportCreator.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.List;
 
 @Service
 public class ReportCreator {
